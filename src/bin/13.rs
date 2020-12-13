0499b4e22f9aa4ad458dbeca4d3c275f906a08c6
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-12-13 15:39:13 +0100

    13 Pt 2 chinese remainder
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -1,21 +1,29 @@
 static INPUT: &str = include_str!("./13.txt");
 
-fn main() {
-    let timestamp = INPUT.lines().next().unwrap().parse::<usize>().unwrap();
-    let bus_lines = INPUT.lines().skip(1).next().unwrap().split(',').enumerate().filter_map(|(idx, bus)| bus.parse::<usize>().ok().map(|bus| (idx, bus))).collect::<Vec<_>>();
-    let (next_bus, next_ts) = bus_lines.iter().map(|(_, bus)| (*bus, (0..).step_by(*bus).skip_while(|t| *t < timestamp).next().unwrap())).min_by_key(|(_, ts)| *ts).unwrap();
-    println!("{}", next_bus * (next_ts - timestamp));
-    for timestamp in (0..).step_by(bus_lines[0].1) {
-        let mut found: bool = true;
-        for (delta, bus) in bus_lines.iter().skip(1) {
-            if (timestamp + delta) % *bus != 0 {
-                found = false;
-                break;
-            }
-        }
-        if found {
-            println!("{}", timestamp);
-            break;
-        }
+fn egcd(a: i64, b: i64) -> (i64, i64, i64) {
+    if a == 0 {
+        (b, 0, 1)
+    } else {
+        let (g, x, y) = egcd(b % a, a);
+        (g, y - (b / a) * x, x)
     }
+}
+
+fn mod_inv(x: i64, n: i64) -> Option<i64> {
+    let (g, x, _) = egcd(x, n);
+    if g == 1 {
+        Some((x % n + n) % n)
+    } else {
+        None
+    }
+}
+
+fn main() {
+    let timestamp = INPUT.lines().next().unwrap().parse::<i64>().unwrap();
+    let bus_lines = INPUT.lines().skip(1).next().unwrap().split(',').enumerate().filter_map(|(idx, bus)| bus.parse::<i64>().ok().map(|bus| (idx as i64, bus))).collect::<Vec<_>>();
+    let (next_bus, next_ts) = bus_lines.iter().map(|(_, bus)| (*bus, (0..).step_by(*bus as usize).skip_while(|t| *t < timestamp).next().unwrap())).min_by_key(|(_, ts)| *ts).unwrap();
+    println!("{}", next_bus * (next_ts - timestamp));
+    let product = bus_lines.iter().map(|(_, bus)| *bus).product::<i64>();
+    let ts = bus_lines.iter().fold(0, |acc, (delta, bus)| acc + (bus - delta) * mod_inv(product / bus, *bus).unwrap() * (product / bus)) % product;
+    println!("{}", ts);
 }
