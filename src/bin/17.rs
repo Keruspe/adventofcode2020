e7e0148e6f7a8b63d4200a93168ad4ca910dabca
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-12-17 10:28:42 +0100

    17
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

---
+++ src/bin/17.rs
@@ -0,0 +1,61 @@
+static INPUT: &str = include_str!("./17.txt");
+
+use std::collections::HashSet;
+
+const INIT_LOOPS: usize = 6;
+
+fn main() {
+    let mut active = HashSet::new();
+    let mut min_x: i64 = 0;
+    let mut min_y: i64 = 0;
+    let mut min_z: i64 = 0;
+    let mut max_x: i64 = 0;
+    let mut max_y: i64 = 0;
+    let mut max_z: i64 = 0;
+    for (x, line) in INPUT.lines().enumerate() {
+        for (y, c) in line.chars().enumerate() {
+            if c == '#' {
+                active.insert((x as i64, y as i64, 0i64));
+            }
+            max_y = y as i64;
+        }
+        max_x = x as i64;
+    }
+    for _ in 0..INIT_LOOPS {
+        min_x -= 1;
+        min_y -= 1;
+        min_z -= 1;
+        max_x += 1;
+        max_y += 1;
+        max_z += 1;
+        let current = active.clone();
+        for x in min_x..=max_x {
+            for y in min_y..=max_y {
+                for z in min_z..=max_z {
+                    let mut active_neighbours = 0;
+                    for xx in (x - 1)..=(x + 1) {
+                        for yy in (y - 1)..=(y + 1) {
+                            for zz in (z - 1)..=(z + 1) {
+                                if xx != x || yy != y || zz != z {
+                                    if current.contains(&(xx, yy, zz)) {
+                                        active_neighbours += 1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if current.contains(&(x, y, z)) {
+                        if active_neighbours != 2 && active_neighbours != 3 {
+                            active.remove(&(x, y, z));
+                        }
+                    } else {
+                        if active_neighbours == 3 {
+                            active.insert((x, y, z));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    println!("{}", active.len());
+}
