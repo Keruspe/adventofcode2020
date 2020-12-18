9585d94c96685f370271cf7f2e1b6395c975852f
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-12-18 10:20:34 +0100

    18 Pt 2
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -52,39 +52,40 @@ impl FromStr for Expr {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut rbegin = match s.rfind(" ") {
-            Some(idx) => idx + 1,
-            None => return Ok(Expr::Value(s.parse().map_err(|_| ())?)),
-        };
-        let mut lend = rbegin - 1;
-        let mut rend = s.len();
-        if s.chars().last().unwrap() == ')' {
-            rend -= 1;
-            let mut parens = 1;
-            for (idx, c) in s.chars().collect::<Vec<_>>().iter().enumerate().rev().skip(1) {
-                if *c == ')' {
-                    parens += 1;
-                } else if *c == '(' {
-                    parens -= 1;
+        if !s.contains(" ") {
+            return Ok(Expr::Value(s.parse().map_err(|_| ())?));
+        }
+        let mut toplevel_add = None;
+        let mut toplevel_mul = None;
+        let mut parens = 0;
+        for (idx, c) in s.chars().enumerate() {
+            match c {
+                '(' => parens += 1,
+                ')' => parens -= 1,
+                '+' => {
+                    if parens == 0 && toplevel_add.is_none() {
+                        toplevel_add = Some(idx);
+                    }
                 }
-                if parens == 0 {
-                    rbegin = idx + 1;
-                    if idx == 0 {
-                        return s[rbegin..rend].parse();
-                    } else {
-                        lend = idx - 1;
+                '*' => {
+                    if parens == 0 {
+                        toplevel_mul = Some(idx);
                         break;
                     }
                 }
+                _ => {}
             }
         }
-        let rhs = &s[rbegin..rend];
-        let (lhs, op) = &s[..lend].rsplit_once(" ").ok_or(())?;
-        Ok(Expr::Operation {
-            lhs: Box::new(lhs.parse()?),
-            op: op.parse()?,
-            rhs: Box::new(rhs.parse()?),
-        })
+        if let Some(idx) = toplevel_mul.or(toplevel_add) {
+            Ok(Expr::Operation {
+                lhs: Box::new(s[0..(idx - 1)].parse()?),
+                op: s[idx..(idx + 1)].parse()?,
+                rhs: Box::new(s[(idx + 2)..].parse()?),
+            })
+        } else {
+            // means we're in toplevel parens, drop them
+            s[1..(s.len() - 1)].parse()
+        }
     }
 }
 
